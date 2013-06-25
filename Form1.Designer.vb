9bd4c74896f498b72ab6a5df4af5ed1388902f55
Giancarlo Saraceni <framelesscomic@gmail.com>
2013-06-25 12:32:02 -0700

    Ta-da!

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.butStart = New System.Windows.Forms.Button()
+        Me.cmbSelect = New System.Windows.Forms.ComboBox()
+        Me.ofdFile = New System.Windows.Forms.OpenFileDialog()
+        Me.cdgBackground = New System.Windows.Forms.ColorDialog()
+        Me.SuspendLayout()
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(25, 9)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(42, 13)
+        Me.lblAnswer.TabIndex = 0
+        Me.lblAnswer.Text = "Answer"
+        '
+        'butStart
+        '
+        Me.butStart.BackColor = System.Drawing.Color.Transparent
+        Me.butStart.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.butStart.Location = New System.Drawing.Point(146, 4)
+        Me.butStart.Name = "butStart"
+        Me.butStart.Size = New System.Drawing.Size(75, 23)
+        Me.butStart.TabIndex = 1
+        Me.butStart.Text = "Decide"
+        Me.butStart.UseVisualStyleBackColor = False
+        '
+        'cmbSelect
+        '
+        Me.cmbSelect.FormattingEnabled = True
+        Me.cmbSelect.Items.AddRange(New Object() {"Ant", "Rnd()", "File"})
+        Me.cmbSelect.Location = New System.Drawing.Point(100, 4)
+        Me.cmbSelect.Name = "cmbSelect"
+        Me.cmbSelect.Size = New System.Drawing.Size(121, 21)
+        Me.cmbSelect.TabIndex = 2
+        Me.cmbSelect.Visible = False
+        '
+        'ofdFile
+        '
+        Me.ofdFile.InitialDirectory = "C:\Program Files\YesNo"
+        Me.ofdFile.ShowReadOnly = True
+        Me.ofdFile.Title = "Choose File"
+        '
+        'cdgBackground
+        '
+        Me.cdgBackground.AnyColor = True
+        Me.cdgBackground.Color = System.Drawing.SystemColors.Control
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(242, 34)
+        Me.Controls.Add(Me.cmbSelect)
+        Me.Controls.Add(Me.butStart)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(258, 69)
+        Me.Name = "Form1"
+        Me.Text = "YesNo Decision Maker"
+        Me.TransparencyKey = System.Drawing.Color.Transparent
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents butStart As System.Windows.Forms.Button
+    Friend WithEvents cmbSelect As System.Windows.Forms.ComboBox
+    Friend WithEvents ofdFile As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents cdgBackground As System.Windows.Forms.ColorDialog
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,196 @@
+﻿Public Class Form1
+    Public bolAnswer As Boolean
+    Public strYes() As String = {"Yes"}
+    Public strNo() As String = {"No"}
+    Public intYesLines As Integer = 0
+    Public intNoLines As Integer = 0
+    Public bolAnt(99, 99) As Boolean
+    Public bytXant As Byte = CByte(100 * Rnd())
+    Public bytYant As Byte = CByte(100 * Rnd())
+    Public bytDant As Byte = CByte(4 * Rnd())
+    Public datClick As Date
+    Public bolIsKeyDown As Boolean = False
+    'Public ftsFlatStyles() As System.Windows.Forms.FlatStyle = {FlatStyle.Flat, FlatStyle.Popup, FlatStyle.Standard} 'Stupid Windows not redrawing the button.
+    'Public bytFstyle As Byte = 2
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Randomize() 'Boss said to make it more random
+      
+        For bytX As Byte = 0 To 99
+            For bytY As Byte = 0 To 99
+                bolAnt(bytX, bytY) = Rnd() >= 0.5
+            Next
+        Next
+        'Try
+        '    Dim objYesReader As New System.IO.StreamReader(My.Resources.Yes)
+        '    'C:\Program Files\YesNo\
+        '    Do Until objYesReader.EndOfStream
+        '        strYes(intYesLines) = objYesReader.ReadLine
+        '        intYesLines += 1
+        '        ReDim Preserve strYes(intYesLines)
+        '        strYes(intYesLines) = "Yes"
+        '    Loop
+        '    objYesReader.Close()
+        'Catch ex As Exception
+        'End Try
+
+        'Try
+        '    Dim objNoReader As New System.IO.StreamReader(My.Resources.No)
+        '    'C:\Program Files\YesNo\
+        '    Do Until objNoReader.EndOfStream
+        '        strNo(intNoLines) = objNoReader.ReadLine
+        '        intNoLines += 1
+        '        ReDim Preserve strNo(intNoLines)
+        '        strNo(intNoLines) = "No"
+        '    Loop
+        '    objNoReader.Close()
+        'Catch ex As Exception
+        'End Try
+        'This is what I get for being clever. I have to scrap the whole thing when Boss2 insists the install instructions are "too complicated" and "shouldn't even be". Sigh.
+
+        strYes = Split(My.Resources.Yes, Environment.NewLine, , CompareMethod.Text)
+        intYesLines = strYes.Length
+        ' intYesLines += 1
+        ReDim Preserve strYes(intYesLines)
+        strYes(intYesLines) = "Yes"
+
+        strNo = Split(My.Resources.No, Environment.NewLine, , CompareMethod.Text)
+        intNoLines = strNo.Length
+        'intNoLines += 1
+        ReDim Preserve strNo(intNoLines)
+        strNo(intNoLines) = "No"
+    End Sub
+    Private Sub butStart_Click(sender As Object, e As EventArgs) Handles butStart.Click
+        datClick = Date.Now
+    End Sub
+    Private Sub Decide(Optional ByRef bytChoose As Byte = 3)
+
+        Select Case bytChoose
+            Case 0
+                GoTo Ant
+            Case 1
+                GoTo Rnd
+            Case 2
+                GoTo FS
+        End Select
+
+
+        If Rnd() <= 0.5 Then
+Rnd:        If Rnd() <= 0.7 Then
+                bolAnswer = bolAnswer = False
+            End If
+        Else
+Ant:        If bolAnt(bytXant, bytYant) Then
+                bytDant = (bytDant + 3) Mod 4
+            Else
+                bytDant = (bytDant + 1) Mod 4
+            End If
+            Select Case bytDant
+                Case 0
+                    bytYant = (bytYant + 3) Mod 100
+                Case 1
+                    bytXant = (bytXant + 1) Mod 100
+                Case 2
+                    bytYant = (bytYant + 1) Mod 100
+                Case 3
+                    bytXant = (bytXant + 3) Mod 100
+            End Select
+            bolAnt(bytXant, bytYant) = bolAnt(bytXant, bytYant) = False
+            bolAnswer = bolAnt(bytXant, bytYant)
+        End If
+        If bytChoose <> 3 Then
+            GoTo YN
+        End If
+
+        GoTo YN
+
+FS:     ofdFile.ShowDialog() 'Boss said it was too random
+        Try
+            Dim objByteReader As New System.IO.StreamReader(ofdFile.FileName)
+            For bytCount As Byte = 0 To 18 'Chosen by blessed d20. Guaranteed to be random.
+                bolAnswer = (CByte(objByteReader.Read) Mod 2) = 1
+            Next
+        Catch ex As Exception
+            If ofdFile.FileName = "" Then
+                MsgBox("Choose a file!", MsgBoxStyle.Exclamation, "Error!")
+                GoTo FS
+            Else
+            End If
+        End Try
+
+YN:     If bolAnswer Then
+            lblAnswer.Text = strYes(CInt(Int((intYesLines + 1) * Rnd())))
+        Else
+            lblAnswer.Text = strNo(CInt(Int((intNoLines + 1) * Rnd())))
+        End If
+    End Sub
+
+    Private Sub butStart_MouseUp(sender As Object, e As MouseEventArgs) Handles butStart.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            cdgBackground.ShowDialog()          'Boss wanted to be more colorful. SIGH.
+            Me.BackColor = cdgBackground.Color
+        ElseIf Date.Now.Subtract(datClick).Milliseconds < 300 Then
+            Decide()
+        ElseIf Date.Now.Subtract(datClick).Milliseconds > 300 And Date.Now.Subtract(datClick).Milliseconds < 1000 Then
+            cmbSelect.Visible = True
+            cmbSelect.Focus()
+        Else
+
+        End If
+    End Sub
+
+    Private Sub cmbSelect_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbSelect.SelectedIndexChanged
+        cmbSelect.Visible = False
+        Decide(CByte(cmbSelect.SelectedIndex))
+        bolIsKeyDown = False 'weirdass bug.
+    End Sub
+
+    Private Sub Form1_MouseWheel(sender As Object, e As MouseEventArgs) Handles MyBase.MouseWheel
+        Dim Rainbow As New Random
+ 
+        Me.BackColor = Color.FromArgb(Rainbow.Next(0, 255), Rainbow.Next(0, 255), Rainbow.Next(0, 255))
+
+        If bolIsKeyDown Then
+            Me.Opacity = (Me.Opacity + WheelTick(e.Delta) - 0.01) Mod 1 + 0.01
+        End If
+    End Sub
+    Private Function WheelTick(ByRef Delta)
+        If Delta > 0 Then
+            Return 0.01
+        ElseIf Delta < 0 Then
+            Return -0.01
+        Else
+            Return 0
+        End If
+    End Function
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        'If e.KeyCode = Keys.Alt Then
+        '    bytFstyle = (bytFstyle + 1) Mod 3
+        'ElseIf e.KeyCode = Keys.Control Then
+        '    bytFstyle = (bytFstyle - 1) Mod 3
+        'Else
+        bolIsKeyDown = True
+        If e.KeyCode = Keys.Home Then
+            Me.Opacity = 1
+        End If
+        If e.KeyCode = Keys.End Then
+            Me.Opacity = 0.01
+        End If
+        'End If
+        'butStart.FlatStyle = ftsFlatStyles(bytFstyle)
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        bolIsKeyDown = False
+    End Sub
+
+    Private Sub Form1_BackColorChanged(sender As Object, e As EventArgs) Handles MyBase.BackColorChanged
+        If 127 < (CInt(Me.BackColor.R) + CInt(Me.BackColor.G) + CInt(Me.BackColor.B)) / 3 Then
+            lblAnswer.ForeColor = Color.Black
+        Else
+            lblAnswer.ForeColor = Color.White
+        End If
+    End Sub
+
+End Class
